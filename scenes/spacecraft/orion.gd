22df5468667c374cad0578a4685a1e293b894448
David Duschek <David-Fred.Duschek@stud.hs-coburg.de>
2026-04-12 13:37:47 +0200

    implemented gravity

---
+++ scenes/spacecraft/orion.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var mass : float = 20e3
+@export var speed : Vector2
+@export var thrust: float 
+@export var turn_speed : float
+@export var angle: float
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
